7266c54d35f20aeae5f7d4d1bf61215560ae8fd7
Manish Kumar <search.manish16@gmail.com>
2019-12-24 17:03:50 +0530

    Initial commit

---
+++ app/src/main/java/com/softcodeinfotech/imageslider/SliderItem.java
@@ -0,0 +1,16 @@
+package com.softcodeinfotech.imageslider;
+
+public class SliderItem {
+
+    //use String variable to use url from internet to get image
+    private int image;
+
+    public SliderItem(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+}
